87864fd2e9b3d4ad9d5503964de29628a8a35f9d
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-01-14 14:59:55 -0800

    started on three

--- q3.js
+++ q3.js
@@ -1,6 +1,13 @@
 // 3. Remove duplicates from an array (using a loop). FYI: array.includes(element) checks if an element is in an array
 function removeDuplicates(array) {
-
+    for (let element in array)
+    {
+        if (array.includes(element))
+        {
+            array.splice(element);
+        }
+    }
+    console.log(array);
 }
 
 // Test for remove duplicates
@@ -8,3 +15,7 @@ function removeDuplicates(array) {
 let testArray3 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]; // [1, 2, 3, 4, 5]
 
 // TODO: display the array with duplicates removed in the console
+
+console.log("1: " + testArray3);
+removeDuplicates("after remove:" + testArray3);
+console.log("2: " + testArray3);
