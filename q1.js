ee0852706bd8ec1e2df3bc1da762649de7c81eed
rjfredr <fredrickalexander1727@gmail.com>
2025-01-14 14:46:33 -0800

    01.14.2025 2:46pm q1 fin

--- q1.js
+++ q1.js
@@ -1,10 +1,16 @@
 // 1. Calculate the sum of all numbers in an array (using a loop)
 
 function sumArray(array) {
-
+    let total = 0;
+    for (let i = 0; i < testArray.length; i++) {
+        total += testArray[i];
+    }
+    console.log(total);
 }
 
 // Test for sum array
 let testArray = [1, 2, 3, 4, 5]; // 15
 
 // TODO: display the sum of the array in the console
+
+sumArray(testArray);
