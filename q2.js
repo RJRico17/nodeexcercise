1c73c7c4ec19a59d28261eec618162ebac544a6f
rjfredr <fredrickalexander1727@gmail.com>
2025-01-14 14:57:34 -0800

    01.14.2025 2:57pm q2 fin

--- q2.js
+++ q2.js
@@ -1,9 +1,17 @@
 // 2. Find the largest number in an array (using a loop)
 function findLargest(array) {
-
+    let largeNum = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > largeNum) {
+            largeNum = array[i];
+        }
+    }
+    console.log(largeNum);
 }
 
 // Test for find largest
 let testArray2 = [1, 4, 5, 4, 3]; // 5
 
 // TODO: display the largest number in the array in the console
+
+findLargest(testArray2);
